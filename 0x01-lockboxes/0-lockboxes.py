a3e21d807d95e148e8f61761ef6d4f8422ad2289
sagebeme <eudicas5k@gmail.com>
2023-07-08 18:02:23 +0300

    pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
+
     """
     creates a set to keep track of all boxes visited
     """
     visited = set()
     visited.add(0)
 
-    stack = [0] # Use a stack to perform depth-first search
-    
+    stack = [0]
+
     # perform DFS
     while stack:
         box = stack.pop()
 
         # check if you have a key to open other boxes
         for key in boxes[box]:
-            if not key in visited:
+            if key not in visited:
                 visited.add(key)
                 stack.append(key)
+
     # check if all boxes have been visited
     return len(visited) == len(boxes)
