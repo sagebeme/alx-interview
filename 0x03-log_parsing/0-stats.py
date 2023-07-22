2faf9c6f10480382f0451adfe485c34677c5d965
sagebeme <eudicas5k@gmail.com>
2023-07-22 13:13:50 +0300

    addind a function that will call the stats instead

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
-
 import sys
 
-def print_statistics(total_size, status_counts):
-    print("File size:", total_size)
-    for status_code in sorted(status_counts):
-        print(f"{status_code}: {status_counts[status_code]}")
+def print_stats(total_size, status_codes):
+    """
+    prints out the status
+    """
+    print("File size: {}".format(total_size))
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code] > 0:
+            print("{}: {}".format(status_code, status_codes[status_code]))
+    print()
 
-def process_logs():
-    total_size = 0
-    status_counts = {}
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-    try:
-        for line_num, line in enumerate(sys.stdin, start=1):
-            try:
-                ip_address, _, _, _, _, status_code, file_size = line.strip().split(' ')
-                status_code = int(status_code)
-                file_size = int(file_size)
-            except ValueError:
-                continue
-
+try:
+    for line in sys.stdin:
+        try:
+            fields = line.strip().split()
+            ip_address = fields[0]
+            status_code = int(fields[-2])
+            file_size = int(fields[-1])
             total_size += file_size
-            status_counts[status_code] = status_counts.get(status_code, 0) + 1
+            status_codes[status_code] += 1
+            line_count += 1
 
-            if line_num % 10 == 0:
-                print_statistics(total_size, status_counts)
-    except KeyboardInterrupt:
-        pass
+            if line_count == 10:
+                print_stats(total_size, status_codes)
+                line_count = 0
 
-    print_statistics(total_size, status_counts)
+        except Exception as e:
+            # Skip this line if we can't parse it
+            pass
 
+except KeyboardInterrupt:
+    # Handle Ctrl-C
+    print_stats(total_size, status_codes)
+    sys.exit(0)
 
-if __name__ == "__main__":
-    process_logs()
+# Print final stats if we don't receive a keyboard interrupt
+print_stats(total_size, status_codes)
