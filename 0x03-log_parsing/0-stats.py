a65889fb21c301ac7767fe2799becf73eb4e231a
sagebeme <eudicas5k@gmail.com>
2023-07-24 18:23:24 +0300

    changing the code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,43 @@
 #!/usr/bin/python3
+""" logs"""
 
 import sys
 
 
-def print_stats(total_size, status_codes):
-    """
-    This function takes in two arguments: total_size,
-    which is the sum of all previous file sizes, and status_codes,
-    which is a dictionary of status codes and the number of times
-    they've appeared in the input.
-    The function prints out the file size and the number of lines
-    for each status code in ascending order.
-    """
-    print("File size: {}".format(total_size))
-    for status_code in sorted(status_codes.keys()):
-        if status_codes[status_code] > 0:
-            print("{}: {}".format(status_code, status_codes[status_code]))
-    print()
+def _print(total_file_size, status):
+    """function to print total file size and status codes"""
+    print("File size: {:d}".format(total_file_size))
+    for key, value in sorted(statuses.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+statuses = {
+        '200': 0, '301': 0,
+        '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
+total_file_size = 0
+count = 0
 try:
     for line in sys.stdin:
-        try:
-            fields = line.strip().split()
-            if len(fields) != 7:
-                continue
-            ip_address = fields[0]
-            status_code = int(fields[-2])
-            file_size = int(fields[-1])
-            total_size += file_size
-            status_codes[status_code] += 1
-            line_count += 1
+        args = line.split()
 
-            if line_count == 10:
-                print_stats(total_size, status_codes)
-                line_count = 0
+        if len(args) > 2:
+            status_code = args[-2]
+            file_size = int(args[-1])
 
-        except Exception as e:
-            # Skip this line if we can't parse it
-            pass
+            if status_code in statuses:
+                statuses[status_code] += 1
+
+            total_file_size += file_size
+            count += 1
+
+            if count == 10:
+                _print(total_file_size, statuses)
+                count = 0
 
 except KeyboardInterrupt:
-    # Handle Ctrl-C
-    print_stats(total_size, status_codes)
-    sys.exit(0)
+    pass
 
-# Print final stats if we don't receive a keyboard interrupt
-print_stats(total_size, status_codes)
+finally:
+    _print(total_file_size, statuses)
