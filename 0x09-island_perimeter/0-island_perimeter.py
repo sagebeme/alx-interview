c2965bd16afbe53aee48313c127e39245cc54e02
sagebeme <eudicas5k@gmail.com>
2023-09-01 12:57:56 +0300

    working on getting the perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    def dfs(r, c):
+        """
+        Purpose: Function that calculates horizontal and vertical connections.
+        """
+        # Check and explore the four neighboring cells
+        if r < 0 or r >= nr or c < 0 or c >= nc or grid[r][c] == 0:
+            return 1  # If out of bounds or water, count as perimeter
+
+        if grid[r][c] == -1:
+            return 0  # If already visited, no additional perimeter
+
+        grid[r][c] = -1  # Mark cell as visited
+
+        perimeter = (
+            dfs(r - 1, c)
+            + dfs(r + 1, c)
+            + dfs(r, c - 1)
+            + dfs(r, c + 1)
+        )
+
+        return perimeter
+
+    nr = len(grid)
+    if nr == 0:
+        return 0
+    nc = len(grid[0])
+    per_island = 0
+    for r in range(nr):
+        for c in range(nc):
+            if grid[r][c] == 1:
+                per_island += dfs(r, c)
+    return per_island
